58de6b2505efc3651e999e23ea1807b66354d8e6
Ahmed Hameed <ahmed.hameed@colorado.edu>
2020-02-20 12:51:47 -0700

    Added/Configured Material UI components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DemoMaterialModule } from './material.module';
+import { HeaderModule } from './header/header.module';
+import { MaterialModule } from './material.module';
+import { BodyModule } from './body/body.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +12,10 @@ import { DemoMaterialModule } from './material.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    DemoMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    HeaderModule,
+    BodyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
