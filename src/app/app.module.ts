f065a0b583009c19749a87a9901be03bfacc247b
Ahmed Hameed <ahmed.hameed@colorado.edu>
2020-02-23 09:27:58 -0700

    Added Material UI Modal for Add/Update ToDo Items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,28 @@ import UserService from './user/user.service';
 import AuthService from './auth/auth.service';
 import { AuthGuard } from './auth/auth.guard';
 import { routing } from './app.routing';
+import { ModalComponent } from './modal/modal.component';
+import { FormsModule } from '@angular/forms';
+import LoadingMaskService from './loading-mask/loading-mask.service';
+import { LoadingMaskComponent } from './loading-mask/loading-mask.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    LoadingMaskComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
     HeaderModule,
     BodyModule,
     routing
   ],
-  providers: [AuthService, AuthGuard, UserService],
+  // entryComponents: [ModalComponent],
+  providers: [AuthService, AuthGuard, UserService, LoadingMaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
