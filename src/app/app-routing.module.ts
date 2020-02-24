f69fc1b8dab2d36c9169571334ae85a32f81a4fa
Ahmed Hameed <ahmed.hameed@colorado.edu>
2020-02-23 23:54:06 -0700

    Moved ng  routes from app.routing.ts(deleted) to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
+import { ListTodoListComponent } from './body/todo-list/list-todo-list/list-todo-list.component';
+import { AboutComponent } from './body/about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'unauth', pathMatch: 'full', component: AuthComponent },
+  { path: '', redirectTo: '/to-do-list', pathMatch: 'full' },
+  { path: 'to-do-list', component: ListTodoListComponent, canActivate: [AuthGuard] },
+  { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
+  { path: '**', component: PageNotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
